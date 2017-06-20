01c8fb5b52c8879c0aeeee9ae6cee3b5ab837271
Hank35 <heincrebolder@gmail.com>
2017-06-20 12:26:55 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,54 @@
+var http = require('http');
+var express = require('express');
+var routes_v1 = require('./api/routes_v1');
+var routes_films = require('./api/routes_films');
+var routes_rentals = require('./api/routes_rentals');
+var bodyParser =  require('body-parser');
+var config = require('./config/config');
+var db = require('./config/db');
+var expressJWT = require('express-jwt');
+var app = express();
+
+
+app.use(bodyParser.urlencoded({ 'extended': 'true' })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+
+app.use(expressJWT({
+    secret: config.secretkey
+}).unless({
+    path: 
+    [
+        { url: '/api/v1/login', methods: ['POST'] },
+        { url: '/api/v1/register', methods: ['POST'] },
+        { url: '/api/v1/film/10', methods: ['GET'] }
+    ]
+}));
+
+app.use('/api/v1', routes_v1);
+app.use('/api/v1', routes_films);
+app.use('/api/v1', routes_rentals);
+
+app.use(function(err, req, res, next) {
+    // console.dir(err);
+    var error = {
+        message: err.message,
+        code: err.code,
+        name: err.name,
+        status: err.status
+    }
+    res.status(401).send(error);
+});
+
+app.use('*', function(req, res) {
+    res.status(400);
+    res.json({
+        'error': 'Deze URL is niet beschikbaar.'
+    });
+});
+
+app.listen(process.env.PORT || 3000, function(){
+	console.log('Server is listening on port 3000');	
+});
+
+module.exports = app;
---
+++ test/server.js
@@ -0,0 +1,22 @@
+/**
+ * Created by dkroeske on 31/03/2017.
+ */
+
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../index');
+var chould = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Hello', function() {
+    it('GET /api/v1/hello', function(done){
+        chai.request(server)
+            .get('/api/v1/hello')
+            .end( function (err, res) {
+                res.should.have.status(401);
+                res.body.should.be.a('object');
+            done();
+        });
+    });
+});
